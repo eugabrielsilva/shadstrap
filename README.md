0201180af7ba692caf99170ad4ba652715394ea2
Gabriel Oliveira <gabriel.o.silva10@gmail.com>
2025-03-10 11:30:28 -0300

    Update components

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
-## Shadstrap
+# Shadstrap
+
+![Version](https://img.shields.io/github/package-json/v/eugabrielsilva/shadstrap?style=for-the-badge&color=blue)
+![Size](https://img.shields.io/github/size/eugabrielsilva/shadstrap/dist%2Fshadstrap.min.css?style=for-the-badge&color=green)
 
 A CSS theme based on [shadcn/ui](https://github.com/shadcn-ui/ui) look, with the benefits of [Bootstrap](https://github.com/twbs/bootstrap) utilities and [FontAwesome](https://github.com/FortAwesome/Font-Awesome) icons.
 
--- src/js/shadstrap.js
+++ src/js/shadstrap.js
@@ -1,4 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
+
+    // Dialog trigger buttons
     document.querySelectorAll('[data-ss-toggle="dialog"]').forEach(el => {
         el.addEventListener('click', (e) => {
             e.preventDefault();
@@ -12,13 +14,16 @@ document.addEventListener('DOMContentLoaded', () => {
                     setTimeout(() => {
                         backdrop.remove();
                     }, 500);
+                    document.body.classList.remove('no-scroll');
                 });
                 targetEl.appendChild(backdrop);
                 targetEl.classList.add('show');
+                document.body.classList.add('no-scroll');
             }
         });
     });
 
+    // Dialog close buttons
     document.querySelectorAll('[data-ss-dismiss="dialog"]').forEach(el => {
         el.addEventListener('click', (e) => {
             e.preventDefault();
@@ -31,10 +36,12 @@ document.addEventListener('DOMContentLoaded', () => {
                         backdrop.remove();
                     }, 500);
                 }
+                document.body.classList.remove('no-scroll');
             }
         });
     });
 
+    // Accordion triggers
     document.querySelectorAll('[data-ss-toggle="accordion"]').forEach(el => {
         el.addEventListener('click', (e) => {
             e.preventDefault();
@@ -42,6 +49,68 @@ document.addEventListener('DOMContentLoaded', () => {
             if(targetEl) {
                 targetEl.classList.toggle('show');
                 el.classList.toggle('open');
+            }
+        });
+    });
+
+    // Sheet trigger buttons
+    document.querySelectorAll('[data-ss-toggle="sheet"]').forEach(el => {
+        el.addEventListener('click', (e) => {
+            e.preventDefault();
+            const targetEl = document.querySelector(el.getAttribute('data-ss-target'));
+            if(targetEl) {
+                const backdrop = document.createElement('div');
+                backdrop.className = 'sheet-backdrop';
+                backdrop.addEventListener('click', (e) => {
+                    e.preventDefault();
+                    targetEl.classList.remove('show');
+                    setTimeout(() => {
+                        backdrop.remove();
+                    }, 500);
+                    document.body.classList.remove('no-scroll');
+                });
+                targetEl.appendChild(backdrop);
+                targetEl.classList.add('show');
+                document.body.classList.add('no-scroll');
+            }
+        });
+    });
+
+    // Sheet close buttons
+    document.querySelectorAll('[data-ss-dismiss="sheet"]').forEach(el => {
+        el.addEventListener('click', (e) => {
+            e.preventDefault();
+            const targetEl = el.closest('.sheet');
+            if(targetEl) {
+                targetEl.classList.remove('show');
+                const backdrop = targetEl.querySelector('.sheet-backdrop');
+                if(backdrop) {
+                    setTimeout(() => {
+                        backdrop.remove();
+                    }, 500);
+                }
+                document.body.classList.remove('no-scroll');
+            }
+        });
+    });
+
+    // Tab toggler buttons
+    document.querySelectorAll('[data-ss-toggle="tab"]').forEach(el => {
+        el.addEventListener('click', (e) => {
+            e.preventDefault();
+            const targetEl = document.querySelector(el.getAttribute('data-ss-target'));
+            if(targetEl) {
+                const parentEl = el.closest('.tabs');
+                if(parentEl) {
+                    parentEl.querySelectorAll('.tab-content').forEach(tab => {
+                        tab.classList.remove('show');
+                    });
+                    parentEl.querySelectorAll('.tab-trigger button').forEach(btn => {
+                        btn.classList.remove('active');
+                    });
+                }
+                el.classList.add('active');
+                targetEl.classList.add('show');
             }
         });
     });
