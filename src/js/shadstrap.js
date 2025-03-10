53a84f00ea026c17cb8544402afd1910ee06370b
Gabriel Oliveira Geodata <gabriel.oliveira.geodata@gmail.com>
2025-03-10 10:05:05 -0300

    Update components

--- src/js/shadstrap.js
+++ src/js/shadstrap.js
@@ -1,27 +1,36 @@
 document.addEventListener('DOMContentLoaded', () => {
     document.querySelectorAll('[data-ss-toggle="dialog"]').forEach(el => {
-        el.addEventListener('click', () => {
+        el.addEventListener('click', (e) => {
+            e.preventDefault();
             const targetEl = document.querySelector(el.getAttribute('data-ss-target'));
             if(targetEl) {
+                const backdrop = document.createElement('div');
+                backdrop.className = 'dialog-backdrop';
+                backdrop.addEventListener('click', (e) => {
+                    e.preventDefault();
+                    targetEl.classList.remove('show');
+                    setTimeout(() => {
+                        backdrop.remove();
+                    }, 500);
+                });
+                targetEl.appendChild(backdrop);
                 targetEl.classList.add('show');
             }
         });
     });
 
-    document.querySelectorAll('.dialog-backdrop').forEach(el => {
-        el.addEventListener('click', () => {
-            const targetEl = el.closest('.dialog');
-            if(targetEl) {
-                targetEl.classList.remove('show');
-            }
-        });
-    });
-
     document.querySelectorAll('[data-ss-dismiss="dialog"]').forEach(el => {
-        el.addEventListener('click', () => {
+        el.addEventListener('click', (e) => {
+            e.preventDefault();
             const targetEl = el.closest('.dialog');
             if(targetEl) {
                 targetEl.classList.remove('show');
+                const backdrop = targetEl.querySelector('.dialog-backdrop');
+                if(backdrop) {
+                    setTimeout(() => {
+                        backdrop.remove();
+                    }, 500);
+                }
             }
         });
     });
